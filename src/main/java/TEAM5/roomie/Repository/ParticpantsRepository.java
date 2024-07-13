10d88e2ab8f01b5a0d9808f6483ffc3eedfa9bae
bluegown <alan0308@naver.com>
2024-07-14 01:27:03 +0900

    empty users

--- src/main/java/TEAM5/roomie/Repository/ParticpantsRepository.java
+++
@@ -1,8 +0,0 @@
-package TEAM5.roomie.Repository;
-
-import TEAM5.roomie.Model.Particpants;
-import TEAM5.roomie.Model.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticpantsRepository  extends JpaRepository<Particpants, Integer> {
-}

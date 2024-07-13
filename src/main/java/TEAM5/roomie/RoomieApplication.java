2b4ee1fac9cd42606c3e6c057a1fdb6882dee640
rhea <dev.rhea4444@gmail.com>
2024-07-14 01:45:43 +0900

    7/14 1:45 conflicts

--- src/main/java/TEAM5/roomie/RoomieApplication.java
+++ src/main/java/TEAM5/roomie/RoomieApplication.java
@@ -2,11 +2,16 @@ package TEAM5.roomie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "TEAM5.roomie.Repository")
+@EntityScan(basePackages = "TEAM5.roomie.Model")
 public class RoomieApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RoomieApplication.class, args);
 	}
 

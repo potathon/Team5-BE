375b8980bf4c0a979faf3cccf2653ecfccee0cba
rhea <dev.rhea4444@gmail.com>
2024-07-13 23:23:13 +0900

    7/13 23:22 user setting

--- src/main/java/TEAM5/roomie/Repository/UserRepository.java
+++ src/main/java/TEAM5/roomie/Repository/UserRepository.java
@@ -1,4 +1,9 @@
 package TEAM5.roomie.Repository;
 
-public interface UserRepository {
+import TEAM5.roomie.Model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
 }

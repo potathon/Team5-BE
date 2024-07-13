99263a72c86fab291deb4d79cf8e2c8b35fe657c
rhea <dev.rhea4444@gmail.com>
2024-07-14 05:45:54 +0900

    7/14 5:44 commit 오류 수정, post 유효성 검사 추가

--- src/main/java/TEAM5/roomie/Model/Posts.java
+++ src/main/java/TEAM5/roomie/Model/Posts.java
@@ -1,6 +1,10 @@
 package TEAM5.roomie.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,30 +24,38 @@ public class Posts {
     private Long id;
 
     @Column(name = "user_name", nullable = false)
+    @Size(max = 10, message = "User name must be less than 10 characters")
     private String user_name;
 
     @Column(name = "user_phone", nullable = false)
+    @Size(max = 20, message = "Phone number must be less than 20 characters")
     private String user_phone;
 
     @Column(name = "title", nullable = false)
+    @NotBlank(message = "Title is mandatory")
     private String title;
 
     @Column(name = "content", nullable = false)
+    @Size(max = 255, message = "Content must be less than 255 characters")
     private String content;
 
     @Column(name = "meet_place", nullable = false)
+    @NotBlank(message = "Meet place is mandatory")
     private String meet_place;
 
     @Column(name = "meet_time", nullable = false)
+    @Future(message = "Meet time must be in the future")
     private String meet_time;
 
     @Column(name = "user_count", nullable = false)
     private int user_count;
 
     @Column(name = "max_count", nullable = false)
+    @Min(value = 1, message = "Max count must be at least 1")
     private int max_count;
 
     @Column(name = "tag", nullable = false)
+    @NotBlank(message = "Tag is mandatory")
     private String tag;
 
     @Column(name = "created_at", nullable = false)
--- src/main/java/TEAM5/roomie/Repository/PostRepository.java
+++ src/main/java/TEAM5/roomie/Repository/PostRepository.java
@@ -2,12 +2,10 @@ package TEAM5.roomie.Repository;
 
 import TEAM5.roomie.Model.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Posts, Integer> {
+public interface PostRepository extends JpaRepository<Posts, Long> {
 
-    List<Posts> findByTag(String groupBuyPosts);
+    List<Posts> findByTag(String tag);
 }
